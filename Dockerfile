ae2a69ac5150fac373e1154e7d6e6bfd905ec977
MiSalah <elbaidoury.salaheddine@gmail.com>
2021-08-20 00:52:37 +0100

    adding docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 # Specify a base image
 FROM node:alpine
 
+# here where the applications files will be moved into in the image that we will build. 
 WORKDIR /usr/app
 
 # Install some dependencies
 COPY ./package.json ./
+
+# the command below will be executed only if the package.json file has been modified
 RUN npm install
+# Copy everything in this folder './' in the working directory './usr/app' specified above
 COPY ./ ./
 
 # Default command
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const redis = require("redis");
 
 const app = express();
+const client = redis.createClient({
+    host : 'redis-server',
+    port : 6379
+});
+
+client.set('visists',0);
 
 app.get('/',(req, res) => {
+    client.get('visists',(err, visists) => {
 
-    res.send('Hi Salaheddine');
+        res.send('Number of visists is ' + visists);
+        client.set('visists', parseInt(visists) + 1 );
+    })
 });
 
 app.listen(8080,()=>{
